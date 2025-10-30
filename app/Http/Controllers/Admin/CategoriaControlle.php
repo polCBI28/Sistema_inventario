dfc3cc69b3867d72fd7defc41f860d1220ce2b07
polCBI28 <pol281999@gmail.com>
2025-10-30 11:07:14 -0500

    inicio

---
+++ app/Http/Controllers/Admin/CategoriaControlle.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Ardmin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+use function Pest\Laravel\call;
+
+class CategoriaControlle extends Controller
+{
+    public function index()
+    {
+        return view ('admin.categoria.index');
+    }
+
+  
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'nombre' => 'required | string| max:255',
+            'descripcion' => 'nullable|text',
+            'estado' => 'required',
+
+        ]);
+
+        try{
+            $validator->validate();
+
+            Categoria::create([
+                'nombre' => $request->nombre,
+                'descripcion' => $request->descripcion,
+                'estado' => $request->estado,
+            ]);
+
+            return redirect()->route('admin.categoria.index')
+                ->with('success','La categoria fue registrado correctamente');
+        }catch(ValidationException $e) {
+            return back()->withErrors($e->validator->errors())->withInput();
+        }
+    }
+
+   
+    public function update(Request $request, string $id)
+    {
+          $validator = Validator::make($request->all(),[
+            'nombre' => 'required | string| max:255',
+            'descripcion' => 'nullable|text',
+            'estado' => 'required',
+
+        ]);
+
+        try{
+            $validator->validate();
+        
+            $categoria = Categoria::findOrFail($id);    
+            Categoria::create([
+                'nombre' => $request->nombre,
+                'descripcion' => $request->descripcion,
+                'estado' => $request->estado,
+            ]);
+
+            return redirect()->route('admin.categoria.index')
+                ->with('success','La categoria fue actualizado correctamente');
+        }catch(ValidationException $e) {
+            return back()->withErrors($e->validator->errors())->withInput();
+        }
+    }
+
+    public function destroy(string $id)
+    {
+        Categoria::find($id)->delete();
+        return redirect() ->route('admin.categoria.index')->with('success','La categoria fu eliminada correctamente.');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\Ardmin\CategoriaControlle;
+use App\Models\Categoria;
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+use Livewire\Volt\Volt;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
+    Volt::route('settings/password', 'settings.password')->name('user-password.edit');
+    Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
+
+    Volt::route('settings/two-factor', 'settings.two-factor')
+        ->middleware(
+            when(
+                Features::canManageTwoFactorAuthentication()
+                    && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
+                ['password.confirm'],
+                [],
+            ),
+        )
+        ->name('two-factor.show');
+});
+
+Route::prefix('Admin')->group(functio() {
+    Route::resouce('categoria', CategoriaControlle::class)
+    ->only(['index','store','update','destroy'])
+    ->name('admin.categoria');
+
+});
